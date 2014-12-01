52739c7d84277705f3532a33fd16e4bb12cc2fd7
Zichao Zhang <zhangzichao17@gmail.com>
2014-12-01 18:23:45 +0100

    use script instead to generate trajectory.

---
+++ des_traj.m
@@ -0,0 +1,19 @@
+time_length = 3;
+interval = 0.02;
+
+t = 0:interval:time_length;
+
+x = sin(2*pi*.125*t - pi/2);
+traj.x = timeseries(x', t);
+
+y = sin(2*pi*.125*t);
+traj.y= timeseries(y', t);
+
+z = (-4)*ones(1, length(t));
+traj.z = timeseries(z', t);
+
+yaw = 0+0.2*(0:interval:1);
+yaw = [zeros(1, length(t)-length(yaw)), yaw];
+traj.yaw = timeseries(yaw', t);
+
+clear time_length interval t x y z yaw
