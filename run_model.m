e3368be780dac017ccb976ca76b653f5bbbb8ed6
Zichao Zhang <zhangzichao17@gmail.com>
2014-12-02 11:18:19 +0100

    add plot enable parameter
    simulation is faster without plot.

--- run_model.m
+++ run_model.m
@@ -2,8 +2,9 @@
 clear; close all; clc;
 
 %% parameters
-sim_time = 20;
+sim_time = 10;
 traj_interval = 0.02;
+enable_plot = true;
 
 %% load model and run
 model = 'sl_quadrotor';
@@ -11,7 +12,7 @@ sim_out = sim(model, 'StopTime', int2str(sim_time));
 
 %% extract simulation result
 des_pitch_roll = sim_out.get('des_pitch_roll');
-des_thrust = sim_out.get('des_thrust');
+des_thrust = sim_out.get('des_thrust'); 
 des_torq = sim_out.get('des_torq');
 des_xy = sim_out.get('des_xy');
 des_yaw = sim_out.get('des_yaw');
