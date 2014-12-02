3593ec963b3d2e9f4c64c24024cdd66a238fd5e6
Zichao Zhang <zhangzichao17@gmail.com>
2014-12-02 15:29:38 +0100

    save result to mat file

--- run_model.m
+++ run_model.m
@@ -1,4 +1,4 @@
-%% run model with this script
+% run model with this script
 clear; close all; clc;
 
 %% parameters
@@ -20,3 +20,6 @@ des_z = sim_out.get('des_z');
 four_rotors = sim_out.get('four_rotors');
 quad_states = sim_out.get('quad_states');
 tout = sim_out.get('tout');
+
+save('result.mat', 'des_pitch_roll', 'des_thrust', 'des_torq', 'des_xy',...
+    'des_yaw', 'des_z', 'four_rotors', 'quad_states', 'tout', 'quad');
