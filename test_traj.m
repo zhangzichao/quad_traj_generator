244c30a7a81f03d5a956c113fbc8cf6e9e343437
Zichao Zhang <zhangzichao17@gmail.com>
2014-12-10 18:44:26 +0100

    minor parameter adjust

--- test_traj.m
+++ test_traj.m
@@ -5,9 +5,9 @@
 clear; close all; clc;
 
 %% set parameters
-dt = 0.1; v = 2.5;
-t1 = 2; t2 = 4; t3 = 6;
-p0 = [0, 0, 1];      % make sure cam in the scene
+dt = 0.01; v = 0.2;
+t1 = 4; t2 = 8; t3 = 12;
+p0 = [0, 0, 3];      % make sure cam in the scene
 orie = [1, 0, 0, 0]; % fixed
 total_len = length(0:0.1:t3);
 traj_matrix = zeros(total_len, 8);
