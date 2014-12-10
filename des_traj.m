5db72fedfc310da9eb5eebde4dd92681f9cee08e
Zichao Zhang <zhangzichao17@gmail.com>
2014-12-10 18:42:54 +0100

    add stright and rotate trajectories

--- des_traj.m
+++ des_traj.m
@@ -5,6 +5,7 @@ interval = traj_interval;
 
 t = 0:interval:time_length;
 
+% trajectory 1: circular
 x = sin(2*pi*.125*t - pi/2);
 traj.x = timeseries(x', t);
 
@@ -17,5 +18,32 @@ traj.z = timeseries(z', t);
 yaw = 0+0.2*(0:interval:8);
 yaw = [zeros(1, length(t)-length(yaw)), yaw];
 traj.yaw = timeseries(yaw', t);
+
+%trajectory 2: straight line
+% x = 0.2 * t;
+% traj.x = timeseries(x', t);
+% 
+% y = zeros(1, length(t));
+% traj.y = timeseries(y', t);
+% 
+% z = (-4)*ones(1, length(t));
+% traj.z = timeseries(z', t);
+% 
+% yaw = zeros(1, length(t));
+% traj.yaw = timeseries(yaw', t);
+
+% trajecory 3: rotate
+% x = zeros(1, length(t));
+% traj.x = timeseries(x', t);
+% 
+% y = zeros(1, length(t));
+% traj.y = timeseries(y', t);
+% 
+% z = zeros(1, length(t));
+% traj.z = timeseries(z', t);
+% 
+% yaw = (2*pi/time_length)*t;
+% traj.yaw = timeseries(yaw', t);
+
 
 clear time_length interval t x y z yaw
