4e1ace072c493c8e09452cf68777e1472ddcd613
Zichao Zhang <zhangzichao17@gmail.com>
2014-12-12 17:56:05 +0100

    covert trajectoy to blender coordinate system
    The transformation script accept Euler angle as roll/pitch/yaw.
    The version of transformation script here uses quternion [w x y z]
    In Blender an old verion of the script uses quternion [x y z w]

--- traj_euler_to_quat.py
+++ traj_euler_to_quat.py
@@ -1,5 +1,6 @@
 import numpy as np
 import transformations as tf
+from math import pi
 import os
 import sys
 
@@ -7,6 +8,21 @@ traj_euler = np.loadtxt("traj_euler.txt")
 open("traj_quaternion.txt", 'w').close
 traj_quaternion = open("traj_quaternion.txt", 'w')
 for row in traj_euler:
-    quaternion = tf.quaternion_from_euler(row[4], row[5], row[6], 'rzyx')
+# convert to z-upward coordinate system
+# only need to transform the position
+    #Rz = tf.rotation_matrix(pi/2, [0, 0, 1])
+    #Rx = tf.rotation_matrix(-pi, [1, 0, 0])
+    #R = tf.concatenate_matrices(Rz, Rx)
+    #RT = np.transpose(R)
+    #R_org = tf.euler_matrix(row[4], row[5], row[6], 'rzyx')
+    #R_total = tf.concatenate_matrices(R, R_org)
+    #R_total = tf.concatenate_matrices(R_total, RT)
+    row[3] = -row[3]
+    row[1], row[2] = row[2], row[1]
+
+# convert to quaternion
+# note: the quaternion order as [x y z w]
+    #quaternion = tf.quaternion_from_matrix(R_total)
+    quaternion = tf.quaternion_from_euler(row[4], row[5], row[6]);
     traj_quaternion.write("%.6f %.4f %.4f %.4f %.4f %.4f %.4f %.4f\n" %(row[0], row[1], row[2], row[3],
-                 quaternion[0], quaternion[1], quaternion[2], quaternion[3]));
+                 quaternion[1], quaternion[2], quaternion[3], quaternion[0]));
