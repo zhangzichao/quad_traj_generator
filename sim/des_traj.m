35bb14e0caa9d0f8b8ed740ba7460c1f4c7fb2af
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-07 12:28:40 +0100

    minor

--- sim/des_traj.m
+++ sim/des_traj.m
@@ -6,34 +6,21 @@ interval = traj_interval;
 t = 0:interval:time_length;
 
 % trajectory 1: circular
-x = sin(2*pi*.125*t - pi/2);
-traj.x = timeseries(x', t);
-
-y = sin(2*pi*.125*t);
-traj.y= timeseries(y', t);
-
-z = (-4)*ones(1, length(t));
-traj.z = timeseries(z', t);
-
-yaw = 0+0.2*(0:interval:8);
-yaw = [zeros(1, length(t)-length(yaw)), yaw];
-traj.yaw = timeseries(yaw', t);
-
-%trajectory 2: straight line
-% x = 0.2 * t;
+% x = sin(2*pi*.125*t - pi/2);
 % traj.x = timeseries(x', t);
 % 
-% y = zeros(1, length(t));
-% traj.y = timeseries(y', t);
+% y = sin(2*pi*.125*t);
+% traj.y= timeseries(y', t);
 % 
-% z = (-4)*ones(1, length(t));
+% z = (-3)*ones(1, length(t));
 % traj.z = timeseries(z', t);
 % 
-% yaw = zeros(1, length(t));
+% yaw = 0+0.2*(0:interval:8);
+% yaw = [zeros(1, length(t)-length(yaw)), yaw];
 % traj.yaw = timeseries(yaw', t);
 
-% trajecory 3: rotate
-% x = zeros(1, length(t));
+%trajectory 2: straight line
+% x = 0.5 * t;
 % traj.x = timeseries(x', t);
 % 
 % y = zeros(1, length(t));
@@ -42,8 +29,21 @@ traj.yaw = timeseries(yaw', t);
 % z = (-3)*ones(1, length(t));
 % traj.z = timeseries(z', t);
 % 
-% yaw = (2*pi/time_length)*t;
+% yaw = zeros(1, length(t));
 % traj.yaw = timeseries(yaw', t);
+
+% trajecory 3: rotate
+x = zeros(1, length(t));
+traj.x = timeseries(x', t);
+
+y = zeros(1, length(t));
+traj.y = timeseries(y', t);
+
+z = (-3)*ones(1, length(t));
+traj.z = timeseries(z', t);
+
+yaw = (2*pi/time_length)*t;
+traj.yaw = timeseries(yaw', t);
 
 
 clear time_length interval t x y z yaw
