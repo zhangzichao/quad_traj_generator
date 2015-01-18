9d0989e65d8f8323646205b987d08b0d47e385ad
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-18 22:47:18 +0100

    stored control as yaw pitch roll(no conversion)
    Conversion is done outside Matlab.

--- utils/save_euler_txt.m
+++ utils/save_euler_txt.m
@@ -11,9 +11,9 @@ traj_matrix(:, 1) = t;
 traj_matrix(:, 2) = quad_states.X.Data;
 traj_matrix(:, 3) = quad_states.Y.Data;
 traj_matrix(:, 4) = quad_states.Z.Data;
-traj_matrix(:, 5) = quad_states.roll.Data;
+traj_matrix(:, 5) = quad_states.yaw.Data;
 traj_matrix(:, 6) = quad_states.pitch.Data;
-traj_matrix(:, 7) = quad_states.yaw.Data;
+traj_matrix(:, 7) = quad_states.roll.Data;
 
 % save
 save('output/txt/traj_euler.txt', 'traj_matrix', '-ascii');
